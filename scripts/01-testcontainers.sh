1a95d797e7b7498fa98e2efb75b8b1207016009c
Luis Gomes <lmagomes@pm.me>
2024-10-07 16:03:09 +0100

    Tweak the testcontainers install script

--- scripts/01-testcontainers.sh
+++ scripts/01-testcontainers.sh
@@ -8,9 +8,10 @@ TEMP_DIR=$(mktemp -d)
     curl -Lo $TEMP_DIR/testcontainers-desktop_linux_x86-64.rpm https://app.testcontainers.cloud/download/testcontainers-desktop_linux_x86-64.rpm
     rpm2cpio $TEMP_DIR/testcontainers-desktop_linux_x86-64.rpm | cpio -idmv
 
-    cp -r $TEMP_DIR/opt/testcontainers-desktop /var/opt/
+    sed -i 's|^Exec=.*|Exec=/usr/bin/testcontainers-desktop|' $TEMP_DIR/usr/share/applications/Testcontainers\ Desktop.desktop
+
+    cp -r $TEMP_DIR/opt/testcontainers-desktop/bin/testcontainers-desktop /usr/bin/testcontainers-desktop
     cp -ru $TEMP_DIR/usr/share/* /usr/share/
 
     gtk-update-icon-cache /usr/share/icons/hicolor
-    ln -sf /var/opt/testcontainers-desktop/bin/testcontainers-desktop /usr/bin/testcontainers-desktop
 )
